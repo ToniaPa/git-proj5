6e7ad19fce20aa50c2c0db53b9894d332c41b59f
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2023-12-11 20:05:38 +0200

    commit 2nd Main.java

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -2,6 +2,6 @@ package gr.aueb.cf.proj5;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Coding Factory from GitProj5 in IntelliJ!!");
+        System.out.println("Hello C... F... from GitProj5 in IntelliJ!!");
     }
 }
