a056a022f91c547def830f59ee4e2739e25d6927
Tonia Papadogiorgaki <papadogiorgaki@outlook.com>
2023-12-15 15:12:19 +0200

    Ta kaname mantara

--- src/gr/aueb/cf/proj5/Main.java
+++ src/gr/aueb/cf/proj5/Main.java
@@ -1,12 +1,18 @@
-package gr.aueb.cf.proj5;
+p
+ackage gr.aueb.cf.proj5;
 
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello C... F... from GitProj5 in IntelliJ!!");
+	  System.out.println("Hello C... F... from GitProj5 in IntelliJ!!");
     }
 
     public static void sayHello() {
         System.out.println("Hello!");
     }
+
+    public static void sayMantara() {
+        System.out.println("Ta kaname mantara   ..!");
+    }
+
 }
